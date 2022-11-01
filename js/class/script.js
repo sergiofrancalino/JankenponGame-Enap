786b773947d990f41fe42bc10d90900c91ecfc61
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-01 19:05:50 -0300

    Finished Choosen

--- js/class/script.js
+++ js/class/script.js
@@ -3,22 +3,65 @@
 class Jankenpon {
   constructor() {
     this.player = "";
-    this.points = 9;
+    this.points = 0;
     this.deck = [
       "./assets/rock.ico",
       "./assets/paper.ico",
       "./assets/scissors.ico",
     ];
-    this.selectedCards = [];
-  }
-  
-  renderDeck() {
     
-    //Surfle array deck 
-    this.deck.sort(() => {
-      return Math.random() - 0.5;
-    });
+  }
 
-    console.log(this.deck);
+  //Compare array deck 
+
+  compareDeck(sorteado, target){
+    
+    if (sorteado == target) {
+      console.log("empate");
+      return "Empate";
+
+     }
+  
+    if (sorteado === "./assets/rock.ico" && target === "./assets/paper.ico") {
+      console.log("Você Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Você Ganhou"
+
+    }
+    if (sorteado === "./assets/rock.ico" && target === "./assets/scissors.ico"){
+      console.log("Maquina Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Machine Win"
+    }
+    if (sorteado === "./assets/paper.ico" && target  === "./assets/rock.ico"){
+      console.log("Maquina Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Machine Win"
+
+    }
+    if (sorteado === "./assets/paper.ico" && target  === "./assets/scissors.ico"){
+      console.log("Você Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Você Ganhou"
+
+    }
+    if (sorteado === "./assets/scissors.ico" && target  === "./assets/rock.ico"){
+      console.log("Você Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Você Ganhou"
+
+    }
+    if (sorteado === "./assets/scissors.ico" && target  === "./assets/paper.ico"){
+      console.log("Máquina Ganhou!");
+      console.log(rock, "Escolha do Usuário - Clicado");
+      console.log (sorteado, "Resultado do sorteador");
+      return "Machine Win"
+    }
   }
 }
+
--- js/dom/index.js
+++ js/dom/index.js
@@ -15,22 +15,81 @@ const board = document.getElementById("board");
 const btnStart = document.getElementById("btnStart");
 
 
-//Add the event listener to btnStart
+//Add the event listener to btnStart at StartScreen class
 btnStart.addEventListener("click", () => {
 
     // Hide the startScreen class 
     startScreen.classList.add("hide");
- 
-    //Show the gameScore class
-    gameScore.className = "show";
 
+     //Show the gameScore class
+    gameScore.className = "show";
     game.player = inputName.value; 
     points.innerText = game.points; 
     playerName.innerText = game.player; 
-  
-    game.renderDeck();
-  
-    settingUpGame();
+    
+    settingUpGame(); //Function -> Let's configure the game
 });
+
+
+//Add the event listener to Rock at Game Screen
+const rock = document.getElementById("rock");
+const resultado = document.getElementById("resultado");
+
+rock.addEventListener("click", (e) => {
+ let result = "";   
+ let sorteado = game.deck [Math.floor(Math.random() * game.deck.length)];
+ let target = e.target.name;
+
+ // game.compareDeck(sorteado,target);
+ result = game.compareDeck(sorteado,target);
+ if (result === "Você Ganhou") {
+    game.points++
+    points.innerText = game.points; 
+ }
+ resultado.innerText = result; 
+ console.log(game.points);
+});
+
+
+    
+
+//Add the event listener to Paper at Game Screen
+btnChoosePaper.addEventListener("click", () => {
+    
+
+   
+ });
+
+ //Add the event listener to ChooseScissors at Game Screen
+btnChooseScissors.addEventListener("click", () => {
+    
+
+
+});
+
+function settingUpGame() {
+    // capturar todas as cardsBack
+    // adicionar a ela um eventlistener
+
+    const allHandGame = document.querySelectorAll(".handGame");
+  
+    allHandGame.forEach((handGame) => {
+      handGame.addEventListener("click", () => {
+        //o que acontece quando a mão é clicada
+  
+        const cardFront = cardBack.previousElementSibling;
+  
+        //mostra a carta da frente
+        cardFront.className = "show cardFront";
+  
+        //esconde a carta de trás (carta verde)
+        cardBack.className = "hide cardBack";
+  
+        game.flipCard(cardFront);
+  
+        points.innerText = game.points
+      });
+    });
+  }
 
   
