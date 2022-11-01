ac0f437df352e350dd134c7bd5ee16c602dea6c1
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-10-31 21:39:52 -0300

    Stating Working 2

--- js/class/script.js
+++ js/class/script.js
@@ -1,8 +1,23 @@
-// CLASS
-// script.js
-$(document).ready(function() {
-    $('#button').click(function() {
-      alert('Hello World!!!    This app is awesome!');
-    });
-  });
+// ALL CLASSES HERE 
+
+class Jankenpon {
+  constructor() {
+    this.player = "";
+    this.points = 9;
+    this.deck = [
+      "./assets/rock.ico",
+      "./assets/paper.ico",
+      "./assets/scissors.ico",
+    ];
+    this.selectedCards = [];
+  }
   
+  renderDeck() {
+    //randomizar o deck ->> EMBARALHAR -> shufle
+    this.deck.sort(() => {
+      return Math.random() - 0.5;
+    });
+
+    console.log(this.deck);
+  }
+}
--- js/dom/index.js
+++ js/dom/index.js
@@ -1,1 +1,38 @@
 // DOM Manipulatation
+
+//Instantiate my Jankenpon class
+const game = new Jankenpon();
+
+//$(document).ready(function() {
+//    $('#button').click(function() {
+//      alert('Hello World!!!    This app is awesome!');
+//    });
+//  });
+
+//Capturing all HTML elements
+const startScreen = document.getElementById("startScreen");
+const inputName = document.getElementById("inputName");
+const gameScreen = document.getElementById("gameScreen");
+const gameScore = document.getElementById("gameScore");
+const playerName = document.getElementById("name");
+const points = document.getElementById("points");
+const board = document.getElementById("board");
+const btnStart = document.getElementById("btnStart");
+
+//adicionar o event listener no btnStart
+btnStart.addEventListener("click", () => {
+
+    // startScreen desapareça
+    startScreen.classList.add("hide");
+ 
+    // mostrar o gameScore
+    gameScore.className = "show";
+  
+    game.player = inputName.value; //setando o nome do jogador na minha classe
+    points.innerText = game.points; // setando o numero de tentativos no meu html
+    playerName.innerText = game.player; // setando o nome do jogador no meu html
+  
+    game.renderDeck();
+  
+    settingUpGame();
+  });
