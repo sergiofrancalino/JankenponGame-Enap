eceeb0bdad2f20843fb405f31b54b4f98457ac53
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-11-03 17:18:23 -0300

    Debugging code w3d4afternoon 1

--- js/dom/index.js
+++ js/dom/index.js
@@ -21,8 +21,9 @@ const btnStart = document.getElementById("btnStart");
 const ranking = document.getElementById("ranking"); //To show "status of play" on screen
 
 const musica = new Audio ("./assets/audio/jogo.mp3");
-// /blob/main/assets/audio/jogo.mp3 no GIT //
 musica.volume = 0.2;
+
+// /blob/main/assets/audio/jogo.mp3 no GIT //
 
 //Step 3 - THE EVENT LISTNER to "btnStart" to show the start game screen
 
